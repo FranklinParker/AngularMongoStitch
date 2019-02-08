ff956f9d877cc09e25889bce6dc6504d47cf3594
Franklin Parker <fparker@deliveryman.com>
2019-02-08 10:40:30 -0500

    all

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+/*
+https://github.com/trashvin/watch-my-budget-app
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'AngularMongoStitch';
+}
