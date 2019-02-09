7d4451be3529ac156d3cbc1f66af18ea721553bf
Franklin Parker <fparker@deliveryman.com>
2019-02-09 16:10:45 -0500

    save

--- src/app/animate/components/animate-home/animate-home.component.ts
+++ src/app/animate/components/animate-home/animate-home.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-animate-home',
   templateUrl: './animate-home.component.html',
-  styleUrls: ['./animate-home.component.scss']
+  styleUrls: ['./animate-home.component.scss'],
+  animations: [
+    
+  ]
 })
 export class AnimateHomeComponent implements OnInit {
 
