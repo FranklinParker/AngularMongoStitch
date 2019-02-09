a8bc22237b51452b2025910c42d017210ee0d7b8
Franklin Parker <fparker@deliveryman.com>
2019-02-09 16:36:08 -0500

    trigger state

--- src/app/animate/components/animate-home/animate-home.component.ts
+++ src/app/animate/components/animate-home/animate-home.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state,style} from '@angular/animations';
 
 @Component({
   selector: 'app-animate-home',
   templateUrl: './animate-home.component.html',
   styleUrls: ['./animate-home.component.scss'],
   animations: [
-    
+    trigger('clickedState', [
+      state('default', style({
+        backgroundColor: 'orange',
+        width: '100px',
+        height: '100px'
+      })),
+      state('clicked', style({
+        backgroundColor: 'blue',
+        width: '300px',
+        height: '50px'
+      }))
+    ])
+
   ]
 })
 export class AnimateHomeComponent implements OnInit {
