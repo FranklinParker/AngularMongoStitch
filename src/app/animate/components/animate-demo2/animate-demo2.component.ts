661546b44a46b0bb872b09f9215e0475068eb882
Franklin Parker <fparker@deliveryman.com>
2019-02-10 16:54:40 -0500

    fix

--- src/app/animate/components/animate-demo2/animate-demo2.component.ts
+++ src/app/animate/components/animate-demo2/animate-demo2.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {showStateTrigger, animateStateTrigger, listStateTrigger} from '../../animation/animationTriggers';
 
 @Component({
