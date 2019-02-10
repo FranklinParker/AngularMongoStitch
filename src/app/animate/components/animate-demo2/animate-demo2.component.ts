2bbeedf5121b9672944e65d974e8a7b3772ac879
Franklin Parker <fparker@deliveryman.com>
2019-02-10 15:08:08 -0500

    fix

--- src/app/animate/components/animate-demo2/animate-demo2.component.ts
+++ src/app/animate/components/animate-demo2/animate-demo2.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-animate-demo2',
   templateUrl: './animate-demo2.component.html',
-  styleUrls: ['./animate-demo2.component.scss']
+  styleUrls: ['./animate-demo2.component.scss'],
+  animations: [
+    trigger('shownState', [
+      state('shown', style({
+        backgroundColor: 'gray',
+      })),
+      transition('void => shown', [
+          style({opacity: 0}),
+          animate(800)
+        ]
+      ),
+      transition('shown => void',
+
+        animate(800, style({opacity: 0}))
+      ),
+    ])
+  ]
 })
 export class AnimateDemo2Component implements OnInit {
+  shown = false;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onClick() {
+    this.shown = !this.shown;
   }
 
 }
