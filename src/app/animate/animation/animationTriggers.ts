0e2631919334da3bf290448ec447cb78275f511f
Franklin Parker <fparker@deliveryman.com>
2019-02-10 15:35:31 -0500

    animate

--- src/app/animate/animation/animationTriggers.ts
+++ src/app/animate/animation/animationTriggers.ts
@@ -64,3 +64,36 @@ export const numberChangedTrigger =
       ]
     )
   ]);
+
+
+export const showStateTrigger = trigger('showState', [
+  // transition('void => *', [
+  //   style({
+  //     opacity: 0
+  //   }),
+  //   animate(300)
+  // ]),
+  // transition('* => void', animate(300, style({
+  //   opacity: 0
+  // })))
+  transition(':enter', [
+    style({
+      opacity: 0
+    }),
+    animate(2000)
+  ]),
+  transition(':leave', animate(500, style({
+    opacity: 0
+  })))
+]);
+
+export const animateStateTrigger = trigger('animateState', [
+  transition('* => *', [
+    animate('4000ms cubic-bezier(.16,.78,0,.92)', style({
+      width: 0
+    })),
+    animate(400, style({
+      width: '*'
+    }))
+  ])
+]);
--- src/app/animate/components/animate-demo2/animate-demo2.component.ts
+++ src/app/animate/components/animate-demo2/animate-demo2.component.ts
@@ -1,26 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import {showStateTrigger, animateStateTrigger} from '../../animation/animationTriggers';
 
 @Component({
   selector: 'app-animate-demo2',
   templateUrl: './animate-demo2.component.html',
   styleUrls: ['./animate-demo2.component.scss'],
   animations: [
-    trigger('shownState', [
-      transition(':enter', [
-          style({opacity: 0}),
-          animate(2000)
-        ]
-      ),
-      transition(':leave',
-
-        animate(800, style({opacity: 0}))
-      ),
-    ])
+    showStateTrigger,
+    animateStateTrigger
   ]
 })
 export class AnimateDemo2Component implements OnInit {
   shown = false;
+  width = 400;
+  animate = false;
 
   constructor() {
   }
