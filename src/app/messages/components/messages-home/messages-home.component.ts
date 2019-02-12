d0752eea520131f43ab2a9125c4bbbe1208f9193
Franklin Parker <fparker@deliveryman.com>
2019-02-12 12:39:12 -0500

    animations

--- src/app/messages/components/messages-home/messages-home.component.ts
+++ src/app/messages/components/messages-home/messages-home.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MessageService} from '../../services/message.service';
 import {Message} from '../../models/message';
-import {itemStateTrigger} from '../../services/animations';
+import {
+  itemStateTrigger,
+  markedTrigger,
+  listStateTrigger
+} from '../../services/animations';
 
-@Component ({
+@Component({
   selector: 'app-messages-home',
   templateUrl: './messages-home.component.html',
   styleUrls: ['./messages-home.component.scss'],
   animations: [
-    itemStateTrigger
+    itemStateTrigger,
+    listStateTrigger,
+    markedTrigger
   ]
 })
 export class MessagesHomeComponent implements OnInit {
   messages: Message[];
-  constructor(private messageService: MessageService) { }
+  selectedMessage: Message;
+
+  constructor(private messageService: MessageService) {
+  }
 
   async ngOnInit() {
-    this.messages = await  this.messageService.getMessages();
+    this.messages = await this.messageService.getMessages();
+  }
+
+  onSelectMessage(message: Message) {
+    this.selectedMessage = message;
   }
 
 }
